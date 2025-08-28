abe2e78c547879e97acb9986ba35c72a4d0d7b42
UESLEI SOUZA DE CARVALHO FILHO <ueslei_carvalho-f@estudante.sesisenai.org.br>
2025-08-28 15:50:41 -0300

    Corrige o funcionamento do menu hamburguer(mobile)

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,6 +1,8 @@
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
+const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", () => {
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 });
